e7b96869687e538a429968d50ae86adcaa08dae4
Abstrask <rask.misc@gmail.com>
2022-01-09 01:35:30 +0100

    Day2 done

--- day2/main.go
+++ day2/main.go
@@ -10,11 +10,13 @@ import (
 )
 
 func main() {
-
 	// Part One
 	_, _, part1Answer := part1("input")
 	fmt.Println("Part One:", part1Answer)
 
+	// Part Two
+	_, _, part2Answer := part2("input")
+	fmt.Println("Part Two:", part2Answer)
 }
 
 func part1(filename string) (int, int, int) {
@@ -45,6 +47,42 @@ func part1(filename string) (int, int, int) {
 			depth += d
 		case "up":
 			depth -= d
+		}
+	}
+
+	return pos, depth, pos * depth
+
+}
+
+func part2(filename string) (int, int, int) {
+
+	readFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var pos, depth, aim int
+
+	for fileScanner.Scan() {
+		i := strings.Split(fileScanner.Text(), " ")
+		o := i[0]                    // operation
+		d, err := strconv.Atoi(i[1]) // delta
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch o {
+		case "forward":
+			pos += d
+			depth += d * aim
+		case "down":
+			aim += d
+		case "up":
+			aim -= d
 		}
 	}
 
--- day2/main_test.go
+++ day2/main_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func TestPartOne(t *testing.T) {
-
 	pos, depth, answer := part1("example")
 	if pos != 15 {
 		t.Errorf("Expected hoz. position of 15, but got %v", pos)
@@ -13,5 +12,18 @@ func TestPartOne(t *testing.T) {
 	}
 	if answer != 150 {
 		t.Errorf("Expected answer of 150, but got %v", answer)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	pos, depth, answer := part2("example")
+	if pos != 15 {
+		t.Errorf("Expected hoz. position of 15, but got %v", pos)
+	}
+	if depth != 60 {
+		t.Errorf("Expected depth of 60, but got %v", depth)
+	}
+	if answer != 900 {
+		t.Errorf("Expected answer of 900, but got %v", answer)
 	}
 }
