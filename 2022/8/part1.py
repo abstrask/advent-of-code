dc51d72a3ed40612979536b4d4f919ebaa5be165
Rasmus Rask <rasmusrask@veo.co>
2022-12-11 20:59:16 +0100

    2022 day 8 part 1

--- 2022/8/part1.py
+++ 2022/8/part1.py
@@ -13,19 +13,42 @@ def populate_tress(lines):
         trees[i] = list(line)
     return trees
 
-
 def solve(input):
     print()
 
     lines = input.splitlines()
-    print(lines)
+    # print(lines)
 
     trees = populate_tress(lines)
+    # print(trees)
 
-    w = len(trees[0])
-    h = len(trees)
+    width = len(trees[0])
+    height = len(trees)
+    count_edge = (2 * width) + (2 * height) - 4
 
-    # ...
+    # iterate over trees
+    visible = 0
+    for (y, row) in enumerate(trees):
+        if y == 0 or y == height - 1:
+            continue  # skip first and last row
+        for (x, tree) in enumerate(row):
+            if x == 0 or x == width - 1:
+                continue  # skip first and last col
+            # print(f"\n{y}, {x}: {tree}")
+
+            left = trees[y][0:x]
+            right = trees[y][x+1:width]
+            # print(f"left: {left}, right: {right}")
+
+            up = [trees[i][x] for i in range(0, y)]
+            down = [trees[i][x] for i in range(y+1, height)]
+            # print(f"up: {up}, down: {down}")
+
+            if tree > max(left) or tree > max(right) or tree > max(up) or tree > max(down):
+                visible += 1
+
+    visible += count_edge
+    return visible
 
 
 def main():
