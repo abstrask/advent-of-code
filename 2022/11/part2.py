941279986e7ceb8eff62db22386e45a0eafc324f
Rasmus Rask <rasmusrask@veo.co>
2023-01-06 23:53:07 +0100

    2022 day 11 part 2 WIP

---
+++ 2022/11/part2.py
@@ -0,0 +1,232 @@
+import re
+import math
+import sys
+import time
+import gmpy2
+# from gmpy2 import mpz
+
+sys.set_int_max_str_digits(50000)
+
+
+class Monkey:
+
+    def __init__(self, items: list[int], operation: str, test_div: int, test_true: int, test_false: int, item_checks: int):
+        self.items = items
+        self.operation = operation
+        self.test_div = test_div
+        self.test_true = test_true
+        self.test_false = test_false
+        self.item_checks = item_checks
+
+    def __repr__(self):
+        return f"items: {self.items},operation: {self.operation}, test_div {self.test_div}, test_true: {self.test_true}, test_false: {self.test_false}, item_checks: {self.item_checks}"
+        # return f"{self.items=}, {self.operation=}, {self.test_div=}, {self.test_true=}, {self.test_false=}, {self.item_checks=}"
+
+    def __str__(self):
+        return f"items: {self.items},operation: {self.operation}, test_div {self.test_div}, test_true: {self.test_true}, test_false: {self.test_false}, item_checks: {self.item_checks}"
+
+
+def main(input):
+    monkeys = init(input)
+    rounds = 10000
+
+    for r in range(1, rounds+1):
+        # print("\n\n")
+        # if r % 100 == 0:
+        if r in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+            print(f"\nRound {r}\n-------------------------")
+        for m, monkey in monkeys.items():
+            # print(f"\n\nMonkey {m} ({monkey.operation})")
+            for i in monkey.items:
+                o = eval(monkey.operation, {"old": i})  # perform operation
+                # b = math.floor(o // 3)  # get bored
+                # if gmpy2.is_divisible(o, monkey.test_div):  # divisible?
+                if divisible(o, monkey.test_div):  # divisible?
+                    monkeys[monkey.test_true].items.append(o)
+                    # print(f"Divisible by {monkey.test_div}, throw to {monkey.test_true}")
+                else:
+                    monkeys[monkey.test_false].items.append(o)
+                    # print(f"Not divisible by {monkey.test_div}, throw to {monkey.test_false}")
+                monkey.item_checks += 1
+            monkey.items = []
+        # print("\n")
+        # if r % 100 == 0:
+        if r in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+            for m, monkey in monkeys.items():
+                print(f"{m}: checks: {monkey.item_checks}")
+
+    # Most active monkeys
+    item_checks = sorted([(v).item_checks for v in monkeys.values()], reverse=True)
+    result = item_checks[0] * item_checks[1]
+    return result
+
+
+def init(input):
+    monkeys = {}
+    for line in input.splitlines():
+        if line.startswith("Monkey "):
+            match = re.search("^Monkey (\d+):$", line)
+            monkey = int(match.group(1))
+        if line.startswith("  Starting items: "):
+            match = re.search("^.*: (.*)$", line)
+            items = [int(i) for i in match.group(1).split(", ")]
+        if line.startswith("  Operation: "):
+            operation = line.split(" = ")[1]
+        if line.startswith("  Test: "):
+            test_div = int(line.split(" by ")[1])
+        if line.startswith("    If true: "):
+            test_true = int(line.split(" monkey ")[1])
+        if line.startswith("    If false: "):
+            test_false = int(line.split(" monkey ")[1])
+            monkeys[monkey] = Monkey(items, operation, test_div, test_true, test_false, 0)
+    # print(monkeys)
+    return monkeys
+
+
+def divisible(x: int, y: int) -> bool:
+
+    match y:
+
+        case 2:
+            return int(str(x)[-1]) % 2 == 0
+
+        # case 5:
+        # https://www.k5learning.com/blog/math-divisibility-rules
+
+        # case 7:
+        # https://www.k5learning.com/blog/math-divisibility-rules
+
+        case 9:
+            return crosssum(x) == 9
+
+        # case 11:
+
+        case 13:
+            # return gmpy2.is_divisible(x, y)
+            # return reduce13(x) % 13 == 0  # 1, 20 ok
+            return reduce_div(x, 13) % 13 == 0  # bad
+
+        case 17:
+            # return gmpy2.is_divisible(x, y)
+            return reduce17(x) % 17 == 0  # 1 ok, 20 bad
+            return reduce_div(x, 17) % 17 == 0  # 1 ok, 20 bad
+
+        case 18:  # This was a mistake - it isn't even necessary ¯\_(ツ)_/¯
+            if divisible(x, 2):
+                return divisible(x, 9)
+            else:
+                return False
+
+        case 19:
+            # return gmpy2.is_divisible(x, y)
+            # return reduce19(x) % 19 == 0  # 1, 20 ok
+            return reduce_div(x, 19) % 19 == 0 # bad
+
+        case 23:
+            # return gmpy2.is_divisible(x, y)
+            # return reduce23(x) % 23 == 0  # 1, 20 ok
+            return reduce_div(x, 23) % 23 == 0  # bad
+
+        case _:
+            if y % 2 == 0:  # if y is even, x should also be divisible by 2, which is a quick check
+                if not divisible(x, 2):
+                    return False
+            else:
+                return x % y == 0
+
+
+def crosssum(x: int) -> int:
+    y = x
+    while True:
+        y = sum([int(a) for a in str(y)])
+        if y < 10:
+            return y
+
+
+# WIP
+def reduce_div(x: int, y: int) -> int:
+    a = x
+    i = 1
+
+    # print(y)
+
+    match y:
+        case 13:
+            digits = 1
+            factor = 4
+
+        case 17:
+            digits = 1
+            factor = 5
+
+        case 19:
+            digits = 2
+            factor = 4
+
+        case 23:
+            digits = 1
+            factor = 7
+
+        case _:
+            return None
+
+    tic = time.perf_counter()
+    while len(str(a)) > (digits + 1):
+        # print(f"{digits=}, {factor=}")
+        b = int(str(a)[-digits:]) * factor  # multiply last digit(s)
+        c = int(str(a)[:-digits])  # get remaining digits
+        a = b + c  # if the sum of the numbers are divisible, so is the original number
+        # print(f"{a=}, {b=}, {c=}")
+        i += 1
+    toc = time.perf_counter()
+    # print(f"reduce {y} finished after {toc - tic:0.2f} seconds {i} iterations")
+    return a
+
+
+def reduce13(x: int) -> int:
+    y = x
+    while len(str(y)) > 2:
+        a = int(str(y)[-1:]) * 4  # multiply last two digit with 4
+        b = int(str(y)[:-1])  # get remaining digits
+        y = a + b  # if the sum of the numbers are divisible by 13, so is the original number
+        # print(f"{y=}, {a=}, {b=}")
+    return y
+
+
+def reduce17(x: int) -> int:
+    y = x
+    while len(str(y)) > 2:
+        a = int(str(y)[-1:]) * 5  # multiply last two digit with 5
+        b = int(str(y)[:-1])  # get remaining digits
+        y = a + b  # if the sum of the numbers are divisible by 17, so is the original number
+    return y
+
+
+def reduce19(x: int) -> int:
+    y = x
+    while len(str(y)) > 3:
+        a = int(str(y)[-2:]) * 4  # multiply last two digit with 4
+        b = int(str(y)[:-2])  # get remaining digits
+        y = a + b  # if the sum of the numbers are divisible by 19, so is the original number
+    return y
+
+
+def reduce23(x: int) -> int:
+    y = x
+    while len(str(y)) > 2:
+        a = int(str(y)[-1]) * 7  # multiply last digit with 7
+        b = int(str(y)[:-1])  # get remaining digits
+        y = (a + b)  # if the sum of the numbers are divisible by 23, so is the original number
+    return y
+
+
+def calc(input):
+    result = []
+
+    return result
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        input = f.read()
+        print(main(input))
---
+++ 2022/11/test_part2.py
@@ -0,0 +1,91 @@
+import unittest
+from parameterized import parameterized_class
+import part2 as part
+
+input = '''\
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+'''
+
+
+part.main(input)
+
+
+# class TestMostActiveProduct(unittest.TestCase):
+#     def test_most_active_product(self):
+#         self.assertEqual(part.main(input), 10605)
+
+
+@parameterized_class(('input', 'expected'), [
+    (123456789, 9),
+    (123456789012, 3),
+])
+class TestCrossSum(unittest.TestCase):
+    def test_cross_sum(self):
+        self.assertEqual(part.crosssum(self.input), self.expected)
+
+
+@parameterized_class(('input', 'expected'), [
+    (pow(13, 5), True),
+    (pow(13, 5) + 3, False),
+])
+class TestReduce13(unittest.TestCase):
+    def test_reduce_13(self):
+        self.assertEqual(part.reduce13(self.input) % 13 == 0, self.expected)
+    def test_reduce_div_13(self):
+        self.assertEqual(part.reduce_div(self.input, 13) % 13 == 0, self.expected)
+
+
+@parameterized_class(('input', 'expected'), [
+    (pow(19, 5), True),
+    (pow(19, 5) + 3, False),
+])
+class TestReduce19(unittest.TestCase):
+    def test_reduce_19(self):
+        self.assertEqual(part.reduce19(self.input) % 19 == 0, self.expected)
+
+
+@parameterized_class(('input', 'expected'), [
+    (pow(23, 5), True),
+    (pow(23, 5) + 3, False),
+])
+class TestReduce23(unittest.TestCase):
+    def test_reduce_23(self):
+        self.assertEqual(part.reduce23(self.input) % 23 == 0, self.expected)
+
+
+# @parameterized_class(('input', 'expected'), [
+#     (input2, 13140),
+# ])
+# class TestSumSigStrength(unittest.TestCase):
+#     def test_sum_sig_strength(self):
+#         self.assertEqual(part.main(self.input), self.expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
