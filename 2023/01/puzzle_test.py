38f5f353e539d5d9926334ac570933bfb3682787
Rasmus Rask <rasmusrask@veo.co>
2023-12-02 00:57:56 +0100

    2023-01 cleaned up tests

--- 2023/01/puzzle_test.py
+++ 2023/01/puzzle_test.py
@@ -1,25 +1,36 @@
 import pytest
-from puzzle import solve_a, solve_b, calc_a, calc_b
+from puzzle import calc_a, solve_a, calc_b, solve_b
 from aocd import get_day_and_year
 from aocd.models import Puzzle
 
 
-# Manual intermediate tests
+# Intermediate tests
 
-def test_calc_a_1():
-    assert calc_a('1abc2') == 12
+calc_a_testdata = [
+    ('1abc2', 12),
+    ('pqr3stu8vwx', 38),
+    ('a1b2c3d4e5f', 15),
+    ('treb7uchet', 77)
+]
+@pytest.mark.parametrize('input,expected', calc_a_testdata)
+def test_calc_a(input, expected):
+    assert calc_a(input) == expected
 
-def test_calc_a_2():
-    assert calc_a('pqr3stu8vwx') == 38
+calc_b_testdata = [
+    ('two1nine', 29),
+    ('eightwothree', 83),
+    ('abcone2threexyz', 13),
+    ('xtwone3four', 24),
+    ('4nineeightseven2', 42),
+    ('zoneight234', 14),
+    ('7pqrstsixteen', 76)
+]
+@pytest.mark.parametrize('input,expected', calc_b_testdata)
+def test_calc_b(input, expected):
+    assert calc_b(input) == expected
 
-def test_calc_a_3():
-    assert calc_a('a1b2c3d4e5f') == 15
 
-def test_calc_a_4():
-    assert calc_a('treb7uchet') == 77
-
-
-# Automatic solution tests
+# Solution tests
 
 day = get_day_and_year()[0]
 year = get_day_and_year()[1]
@@ -28,6 +39,7 @@ p = Puzzle(day=day, year=year)
 test_a = []
 test_b = []
 for e in p.examples:
+    print(e)
     if e.answer_a:
         test_a.append((e.input_data, e.answer_a))
     if e.answer_b:
@@ -37,7 +49,14 @@ for e in p.examples:
 def test_solve_a(input, expected):
     assert solve_a(input) == expected
 
-@pytest.mark.parametrize('input,expected', test_b)
-def test_solve_b(input, expected):
-    assert solve_b(input) == expected
+# EXAMPLE NOT UPDATED!
+example_b = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+def test_solve_b():
+    assert solve_b(example_b) == "281"
 
