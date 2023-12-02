364c67f0a7f5fb726e7204a05bf5129a47614a79
Rasmus Rask <rasmusrask@veo.co>
2023-12-02 11:57:49 +0100

    2023-02

---
+++ 2023/02/puzzle.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# import regex as re
+import re
+from aocd import data, get_day_and_year
+from aocd.models import Puzzle
+
+
+def calc_a(input):
+    TOTAL_RED = 12
+    TOTAL_GREEN = 13
+    TOTAL_BLUE = 14
+    parts = input.split(': ')
+    game = int(re.match(r'^Game (\d+)', parts[0]).group(1))
+    possible = True
+    sets = []
+    sets_str = parts[1].split('; ')
+    for s in sets_str:
+        try:
+            red = int(re.search(r'(\d+) red', s).group(1))
+            if red > TOTAL_RED:
+                possible = False
+        except AttributeError:
+            red = 0
+        try:
+            green = int(re.search(r'(\d+) green', s).group(1))
+            if green > TOTAL_GREEN:
+                possible = False
+        except AttributeError:
+            green = 0
+        try:
+            blue = int(re.search(r'(\d+) blue', s).group(1))
+            if blue > TOTAL_BLUE:
+                possible = False
+        except AttributeError:
+            blue = 0
+        # sets.append({
+        #     'red': red,
+        #     'green': green,
+        #     'blue': blue
+        # })
+    # print(f'\n{game=} {possible=}')
+    result = {
+        'game_id': game,
+        'possible': possible
+    }
+    return result
+
+
+def solve_a(input):
+    result = 0
+    # print(input)
+    for line in input.split('\n'):
+        game = calc_a(line)
+        if game['possible']:
+            result += game['game_id']
+    return str(result)
+
+
+def calc_b(input):
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+    parts = input.split(': ')
+    sets_str = parts[1].split('; ')
+    for s in sets_str:
+        try:
+            red = int(re.search(r'(\d+) red', s).group(1))
+            min_red = max(red, min_red)
+        except AttributeError:
+            red = 0
+        try:
+            green = int(re.search(r'(\d+) green', s).group(1))
+            min_green = max(green, min_green)
+        except AttributeError:
+            green = 0
+        try:
+            blue = int(re.search(r'(\d+) blue', s).group(1))
+            min_blue = max(blue, min_blue)
+        except AttributeError:
+            blue = 0
+    # print(f'{min_red=} {min_green=} {min_blue=}')
+    result = min_red * min_green * min_blue
+    return result
+
+
+def solve_b(input):
+    result = 0
+    for line in input.split('\n'):
+        result += calc_b(line)
+    return str(result)
+
+
+def main():
+    day = get_day_and_year()[0]
+    year = get_day_and_year()[1]
+    p = Puzzle(day=day, year=year)
+
+    result_a = solve_a(data)
+    print(f'Answer A: {result_a}')
+    p.answer_a = result_a
+
+    result_b = solve_b(data)
+    print(f'Answer B: {result_b}')
+    p.answer_b = result_b
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2023/02/puzzle_test.py
@@ -0,0 +1,72 @@
+import pytest
+from puzzle import calc_a, solve_a, calc_b, solve_b
+from aocd import get_day_and_year
+from aocd.models import Puzzle
+
+# Setup
+
+day = get_day_and_year()[0]
+year = get_day_and_year()[1]
+p = Puzzle(day=day, year=year)
+
+test_a = []
+test_b = []
+for e in p.examples:
+    print(e)
+    if e.answer_a:
+        test_a.append((e.input_data, e.answer_a))
+    if e.answer_b:
+        test_b.append((e.input_data, e.answer_b))
+
+if test_a:
+    for t in test_a:
+        print(f'\nInput A:\n{t[0]}')
+        print(f'Expected A: {t[1]}')
+if test_b:
+    for t in test_b:
+        print(f'\nInput B:\n{t[0]}')
+        print(f'Expected B: {t[1]}')
+
+
+# Intermediate tests
+
+calc_a_testdata = [
+    ('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', True),
+    ('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', True),
+    ('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', False),
+    ('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', False),
+    ('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', True)
+]
+@pytest.mark.parametrize('input,expected', calc_a_testdata)
+def test_calc_a(input, expected):
+    assert calc_a(input)['possible'] == expected
+
+calc_b_testdata = [
+    ('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', 48),
+    ('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', 12),
+    ('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 1560),
+    ('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 630),
+    ('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', 36)
+]
+@pytest.mark.parametrize('input,expected', calc_b_testdata)
+def test_calc_b(input, expected):
+    assert calc_b(input) == expected
+
+
+# Solution tests
+
+range_a = range(0, len(test_a))
+@pytest.mark.parametrize('input,expected', test_a, ids=range_a)
+def test_solve_a(input, expected):
+    assert solve_a(input) == expected
+
+# EXAMPLE NOT UPDATED!
+example_b = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+range_b = range(0, len(test_b))
+def test_solve_b():
+    assert solve_b(example_b) == "2286"
