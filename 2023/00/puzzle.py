3aa333d4619e9935d5630fcc72f547f6442e7b1e
Rasmus Rask <rasmusrask@veo.co>
2023-12-02 01:54:12 +0100

    Template, update gitignore

---
+++ 2023/00/puzzle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from aocd import data, get_day_and_year
+from aocd.models import Puzzle
+
+
+def calc_a(input):
+    result = '0'
+    return int(result)
+
+
+def solve_a(input):
+    result = 0
+    for line in input.split('\n'):
+        result += calc_a(line)
+    return str(result)
+
+
+def calc_b(input):
+    result = '0'
+    return int(result)
+
+
+def solve_b(input):
+    result = 0
+    for line in input.split('\n'):
+        result += calc_b(line)
+    return str(result)
+
+
+def main():
+    day = get_day_and_year()[0]
+    year = get_day_and_year()[1]
+    p = Puzzle(day=day, year=year)
+
+    result_a = solve_a(data)
+    print(f'Answer A: {result_a}')
+    p.answer_a = result_a
+
+    result_b = solve_b(data)
+    print(f'Answer B: {result_b}')
+    p.answer_b = result_b
+
+
+if __name__ == '__main__':
+    main()
